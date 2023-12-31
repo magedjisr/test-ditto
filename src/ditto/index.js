4d4ac377a5e314220c1a36d9aef7576e5194004e
magedjisr <maged@jisr.net>
2023-12-31 17:52:06 +0530

    pull test project

---
+++ src/ditto/index.js
@@ -0,0 +1,17 @@
+import components__root__arabic_1 from './components__root__arabic-1.json';
+import components__root__arabic from './components__root__arabic.json';
+import components__root__base from './components__root__base.json';
+import test_modal from './test-modal__arabic-1.json';
+import test_modal1 from './test-modal__base.json';
+
+export default {
+  "project_658d53e5a472bb7eeddf13b2": {
+    "arabic-1": {...test_modal},
+    "base": {...test_modal1}
+  },
+  "ditto_component_library": {
+    "arabic-1": {...components__root__arabic_1},
+    "arabic": {...components__root__arabic},
+    "base": {...components__root__base}
+  }
+}
