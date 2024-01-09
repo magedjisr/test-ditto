68298899a6bbffb90d469d5ad361a47cd9aa3c1b
magedjisr <maged@jisr.net>
2024-01-09 12:03:13 +0530

    updates config

--- src/ditto/index.js
+++ src/ditto/index.js
@@ -1,17 +1,18 @@
-import components__root__arabic_1 from './components__root__arabic-1.json';
-import components__root__arabic from './components__root__arabic.json';
-import components__root__base from './components__root__base.json';
-import test_modal from './test-modal__arabic-1.json';
-import test_modal1 from './test-modal__base.json';
+import components__actions__arabic from './components__actions__arabic.json';
+import components__actions__base from './components__actions__base.json';
+import components__confirmation_modals__base from './components__confirmation-modals__base.json';
+import components__date_pickers__arabic from './components__date-pickers__arabic.json';
+import components__date_pickers__base from './components__date-pickers__base.json';
+import components__navigation__arabic from './components__navigation__arabic.json';
+import components__navigation__base from './components__navigation__base.json';
+import components__pickers__arabic from './components__pickers__arabic.json';
+import components__pickers__base from './components__pickers__base.json';
+import components__text_fields__arabic from './components__text-fields__arabic.json';
+import components__text_fields__base from './components__text-fields__base.json';
 
 export default {
-  "project_658d53e5a472bb7eeddf13b2": {
-    "arabic-1": {...test_modal},
-    "base": {...test_modal1}
-  },
   "ditto_component_library": {
-    "arabic-1": {...components__root__arabic_1},
-    "arabic": {...components__root__arabic},
-    "base": {...components__root__base}
+    "arabic": {...components__actions__arabic, ...components__date_pickers__arabic, ...components__navigation__arabic, ...components__pickers__arabic, ...components__text_fields__arabic},
+    "base": {...components__actions__base, ...components__confirmation_modals__base, ...components__date_pickers__base, ...components__navigation__base, ...components__pickers__base, ...components__text_fields__base}
   }
 }
